07ffad5145935210615c871c9014e7b402ead079
Pedro Rey Anca <pedroreya@gmail.com>
2021-08-29 16:16:17 +0200

    Lots of changes. Forgot I was using git

---
+++ Assets/Scripts/GoBack.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBack : MonoBehaviour
+{
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Escape))
+      {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+          Application.Quit();
+        }
+        else
+        {
+          SceneManager.LoadScene(0);
+        }
+      }
+    }
+}
